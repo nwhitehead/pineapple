1b8c02fd55c7a7eedfff1a921068f8884b66a24f
Nathan Whitehead <nwhitehe@gmail.com>
2015-08-08 00:52:17 -0700

    Spawn new windows from special title changes.

--- server.py
+++ server.py
@@ -17,6 +17,7 @@ import wx.html2
 class CustomBrowser(wx.Dialog):
     def __init__(self, *args, **kwargs):
         wx.Dialog.__init__(self, *args, **kwargs)
+        self.children = []
         sizer = wx.BoxSizer(wx.VERTICAL)
         self.view = wx.html2.WebView.New(self)
         sizer.Add(self.view, 1, wx.EXPAND, 10)
@@ -29,14 +30,17 @@ class CustomBrowser(wx.Dialog):
         self.Bind(wx.html2.EVT_WEBVIEW_NEWWINDOW, self.OnNewWindow)
         self.Bind(wx.html2.EVT_WEBVIEW_TITLE_CHANGED, self.OnTitleChanged, self.view)
 
+    def Spawn(self, url):
+        child = CustomBrowser(None, -1)
+        child.LoadURL(url)
+        child.Show()
+        self.children.append(child)
+
     def OnCloseWindow(self, event):
         self.Destroy()
 
     def OnNavigate(self, event):
         print('Navigate [{}][{}]'.format(event.GetTarget(), event.GetURL()))
-        
-        if event.GetTarget() != '':
-            event.Veto()
 
     def OnLoaded(self, event):
         print('Loaded {}{}'.format(event.GetTarget(), event.GetURL()))
@@ -45,12 +49,15 @@ class CustomBrowser(wx.Dialog):
         print('WebView error')
 
     def OnNewWindow(self, event):
-        print('New window requested [{}]'.format(event.GetURL()))
+        url = event.GetURL()
+        print('New window requested [{}]'.format(url))
+        self.Spawn(url)
 
     def OnTitleChanged(self, event):
         txt = event.GetString()
-        if txt[:3] == 'LOG':
-            print('CONSOLE LOG: {}'.format(txt))
+        if txt[:4] == '$$$$':
+            url = txt[4:]
+            self.Spawn('http://localhost:8888' + url)
             return
         print('Title changed to {}'.format(event.GetString()))
     
