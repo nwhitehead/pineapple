585de668813337355d491bbcf1bf57136655131e
Nathan Whitehead <nwhitehe@gmail.com>
2015-08-09 09:11:17 -0700

    New standalone test app to verify wxwidget installation.

---
+++ wxnotebook/src/app.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+
+#include <wx/wxprec.h>
+
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+
+class MyApp: public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+class MyFrame: public wxFrame
+{
+public:
+    MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+
+private:
+    void OnHello(wxCommandEvent &event);
+    void OnExit(wxCommandEvent &event);
+    void OnAbout(wxCommandEvent &event);
+    
+    wxDECLARE_EVENT_TABLE();
+};
+
+enum
+{
+    ID_Hello = 1
+};
+
+wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
+    EVT_MENU(ID_Hello, MyFrame::OnHello)
+    EVT_MENU(wxID_EXIT, MyFrame::OnExit)
+    EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
+wxEND_EVENT_TABLE()
+
+wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit()
+{
+    MyFrame *frame = new MyFrame("Hello world", wxPoint(50, 50), wxSize(400, 400));
+    frame->Show();
+    wxVersionInfo vi = wxGetLibraryVersionInfo();
+    std::cout << "OnInit() vi=" << vi.ToString() << std::endl;
+    return true;
+}
+
+MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
+    : wxFrame(nullptr, wxID_ANY, title, pos, size)
+{
+
+}
+
+void MyFrame::OnExit(wxCommandEvent &event)
+{
+    Close(true);
+}
+
+void MyFrame::OnAbout(wxCommandEvent &event)
+{
+    wxMessageBox("This is wxWidgets",
+        "About", wxOK | wxICON_INFORMATION);
+}
+
+void MyFrame::OnHello(wxCommandEvent &event)
+{
+    wxLogMessage("Hello world");
+}
